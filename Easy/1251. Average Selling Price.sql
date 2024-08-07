35b9f0ba56d96e1f341494ccb52d34b2d45c123d
SENTHIL <123928930+hedge-hog-23@users.noreply.github.com>
2024-08-07 14:37:19 +0530

    Create 1251. Average Selling Price.sql

---
+++ Easy/1251. Average Selling Price.sql
@@ -0,0 +1,5 @@
+select p.product_id, IFNULL(round(sum(p.price * u.units)/sum(u.units),2),0) 
+as average_price 
+from prices p left join unitssold u on p.product_id = u.product_id 
+and 
+u.purchase_date between start_date and end_date group by product_id;
